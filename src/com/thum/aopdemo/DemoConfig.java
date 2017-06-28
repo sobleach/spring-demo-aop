1593932b486d15628453c4df5a7f2a011e38e90a
sobleach <suchat.nu@gmail.com>
2017-06-28 23:33:24 +0700

    init project for AOP

---
+++ src/com/thum/aopdemo/DemoConfig.java
@@ -0,0 +1,12 @@
+package com.thum.aopdemo;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+@Configuration
+@EnableAspectJAutoProxy
+@ComponentScan("com.thum.aopdemo")
+public class DemoConfig {
+
+}
---
+++ src/com/thum/aopdemo/dao/MembershipDAO.java
@@ -0,0 +1,13 @@
+package com.thum.aopdemo.dao;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class MembershipDAO {
+
+	public void addAccount() {
+		
+		System.out.println(getClass() + ": DOING STUFF: ADDING A MEMBERSHIP ACCOUNT");
+		
+	}
+}
